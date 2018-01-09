0cc04d1930c697d41fe5ae373301c44f086dd241
Arthur Lui <luiarthur@gmail.com>
2018-01-09 13:38:03 -0800

    yes

--- recommender/src/Recommender.scala
+++ recommender/src/Recommender.scala
@@ -7,8 +7,8 @@ object Recommender {
     def classify() = {
       require(score >= 0 && score <=5)
       score match {
-        case x if x <= 2.5 => -1
-        case x if x >= 3.5 => 1
+        case x if x <= 2 => -1
+        case x if x >= 4 => 1
         case _ => 0
       }
     }
@@ -27,24 +27,42 @@ object Recommender {
     Movie(s(0).toInt, s(1), s(2).split('|').toSet)
   }.toSet
 
+  val movieMap = movies.map(m => (m.id, m.title)).toMap
+
   val reviews = readFile("ratings.csv").toList.tail.map{ line =>
     val s = line.split(',')
     Review(s(0).toInt, s(1).toInt, s(2).toDouble)
   }
 
-  /* Movie IDs:
-   * - Toy Story: 2253
-   * - Batman Begins: 33794
-   */
 
-  /*
-  val review = Review(0, 33794, 4.5)
-  */
-
-  def recommend(review:Review, numMovies:Int=3, atLeast:Double=4) = {
+  def recommend(review:Review, numMovies:Int=3) = {
     val similarReviews = reviews.filter(_.similar(review))
-    val similarUsers = relevantReviews.map(_.userId).distinct
-    //val relevantReviews = reviews.filter()
-    ???
+    val similarUsers = similarReviews.map(_.userId).distinct
+    val relevantReviews = reviews.filter(r => similarUsers.contains(r.userId))
+
+    val counts = collection.mutable.Map[Int,Int]().withDefaultValue(0)
+    val scores = collection.mutable.Map[Int,Double]().withDefaultValue(0)
+
+    relevantReviews.foreach{ r => 
+      counts(r.movieId) += 1 
+      scores(r.movieId) += r.score
+    }
+    scores.keys.foreach{ k => scores(k) /= counts(k) }
+
+    //scores.foreach(println)
+    scores.toList.sortBy(-_._2).take(numMovies).map(m => (movieMap(m._1),m._2))
   }
 }
+
+/* Movie IDs:
+ * - Toy Story: 2253
+ * - Batman Begins: 33794
+ */
+
+/* Test
+import Recommender._
+recommend(Review(0, 33794, 0), 10).foreach{println}
+recommend(Review(0, 33794, 1), 10).foreach{println}
+recommend(Review(0, 33794, 5), 10).foreach{println}
+val x = reviews.filter(_.movieId == 33794).map(_.classify)
+*/
