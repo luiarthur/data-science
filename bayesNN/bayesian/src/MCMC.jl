a9558c693d3b8e6f9211d9de8f698944eb5c87a9
Arthur Lui <luiarthur@gmail.com>
2018-02-10 18:20:32 -0800

    yes

--- bayesNN/bayesian/src/MCMC.jl
+++ bayesNN/bayesian/src/MCMC.jl
@@ -54,3 +54,43 @@ function metropolis(curr::Vector{Float64}, ll::Function, lp::Function, cs::Float
 
   return new_state
 end
+
+function hmc(curr::Vector{Float64}, U::Function, grad_U::Function,
+             eps::Float64, L::Int)::Vector{Float64}
+  const M = length(curr)
+  q = curr
+  p = randn(M)
+
+  # Make half a step for momentum at beginning 
+  p -= eps * grad_U(q) / 2
+  
+  # Alternate full steps for position and momentum
+  for i in 1:L
+    # Make a full step for the position
+    q += eps * p
+    # Make a full step for the momentum, except at end of trajectory
+    if i <L
+      p -= eps * grad_U(q)
+    else
+      # Make a half step for momentum at the end.
+      p -= eps * grad_U(q) / 2
+    end
+  end
+
+  # Negate momentum at end of trajectory to make the proposal symmetric
+  p = -p
+
+  # Evaluate potential and kinetic energies at start and end of trajectory
+  const current_U = U(current_q)
+  const current_K = sum(current_p .^ 2) / 2
+  const proposed_U = U(q)
+  const proposed_K = sum(p .^ 2) / 2
+
+  # Accept or reject the state at end of trajectory, returning either
+  # the position at the end of the trajectory or the initial position
+  if current_U - proposed_U + current_K - proposed_K > log(rand())
+    return q # accept
+  else
+    return current_q # reject
+  end
+end
