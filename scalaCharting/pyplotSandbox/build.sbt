e9af31a7f1e15f884e43808ee828cec4524dba5b
Arthur Lui <luiarthur@gmail.com>
2018-09-21 06:41:08 -0700

    add matplotlib4j test

---
+++ scalaCharting/pyplotSandbox/build.sbt
@@ -0,0 +1,11 @@
+name := "pyplotSandbox"
+
+version := "0.1.0"
+
+scalaVersion := "2.12.6"
+
+libraryDependencies ++= Seq(
+  "com.github.sh0nk" % "matplotlib4j" % "0.4.0",
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+)
+
---
+++ scalaCharting/pyplotSandbox/src/main/scala/Bla.scala
@@ -0,0 +1,10 @@
+import com.github.sh0nk.matplotlib4j._
+import scala.collection.JavaConverters._
+
+object Bla {
+  println("Here")
+
+  // FIXME!
+  // See scala example here: https://github.com/sh0nk/matplotlib4j
+  val plt = new PlotImpl()
+}
