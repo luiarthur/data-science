606b2845508f3127406b40711cf05c74b5c09455
Arthur Lui <luiarthur@gmail.com>
2018-09-21 07:46:09 -0700

    added scatter plot example Trying to make heatmap

--- scalaCharting/pyplotSandbox/src/main/scala/Bla.scala
+++ scalaCharting/pyplotSandbox/src/main/scala/Bla.scala
@@ -1,10 +1,28 @@
-import com.github.sh0nk.matplotlib4j._
-import scala.collection.JavaConverters._
+import com.github.sh0nk.matplotlib4j
+import scala.collection.JavaConverters._ // .asJava
 
 object Bla {
   println("Here")
+  val rng = new scala.util.Random(0)
 
-  // FIXME!
-  // See scala example here: https://github.com/sh0nk/matplotlib4j
-  val plt = new PlotImpl()
+  // See example here: https://github.com/sh0nk/matplotlib4j
+
+  // Scatter plot
+  val plt = matplotlib4j.Plot.create()
+  plt.title("Bob")
+  val n = 1000
+  val x = List.tabulate(n)( _ => rng.nextGaussian ).map{ Double.box }.asJava
+  val y = List.tabulate(n)( _ => rng.nextGaussian ).map{ Double.box }.asJava
+  plt.plot.add(x, y, "o")
+  // plt.show() // shows plot immediately
+  plt.savefig("here.pdf") // plots to project top dir
+  plt.executeSilently() // necessary to make plot
+
+  // Heatmap?
+  //val plt2 = matplotlib4j.Plot.create()
+  //val xx = List.range(0, 5)
+  //val yy = List.range(0, 5)
+  //val z = for (x <- xx; y <- yy) yield x + y + 1.0
+  //val xyT = xy.transpose.map{ _.map{zz => Double.box(zz)}.toList.asJava }
+  //plt2.pcolor.add(xx, yy, z).cmap("plt.cm.Blues")
 }
